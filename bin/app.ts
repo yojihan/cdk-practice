74fe9ccc168afe0716005bceca4d2f2ad15811bc
yojihan <141936250+yojihan@users.noreply.github.com>
2024-10-11 00:15:06 +0000

    setup eslint and prettier

--- bin/app.ts
+++ bin/app.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from 'aws-cdk-lib';
-import { AppStack } from '../lib/app-stack';
+import "source-map-support/register";
+import * as cdk from "aws-cdk-lib";
+import { AppStack } from "../lib/app-stack";
 
 const app = new cdk.App();
-new AppStack(app, 'AppStack', {});
+new AppStack(app, "AppStack", {});
---
+++ eslint.config.mjs
@@ -0,0 +1,9 @@
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+
+export default [
+  {files: ["**/*.{js,mjs,cjs,ts}"]},
+  {languageOptions: { globals: {...globals.browser, ...globals.node} }},
+  ...tseslint.configs.recommended,
+];
