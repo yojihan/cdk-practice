1d0f334062f1f8e3de7885996373966f7626f1f3
yojihan <141936250+yojihan@users.noreply.github.com>
2024-10-11 01:20:06 +0000

    set default env

--- bin/app.ts
+++ bin/app.ts
@@ -1,7 +1,12 @@
 #!/usr/bin/env node
 import "source-map-support/register";
 import * as cdk from "aws-cdk-lib";
-import { AppStack } from "../lib/app-stack";
+import { AppStack } from "../lib/stacks/app-stack";
 
 const app = new cdk.App();
-new AppStack(app, "AppStack", {});
+new AppStack(app, "AppStack", {
+  env: {
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: process.env.CDK_DEFAULT_REGION,
+  },
+});
---
+++ lib/stacks/app-stack.ts
@@ -0,0 +1,9 @@
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+export class AppStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+  }
+}
